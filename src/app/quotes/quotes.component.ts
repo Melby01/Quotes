060de4bd1a6beab017b0b7d7ec24f84a29b095b5
Melby01 <melby.okozi@student.moringaschool.com>
2021-05-15 18:39:23 +0300

    Update the quotes.component.ts

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,6 +7,13 @@ import { Quotes } from '../quotes';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  
+  quotes: Quotes[] = [
+    {id:1,name:'Your time is limited, so do not waste it living someone else life. Do not be trapped by dogma – which is living with the results of other peoples thinking'},
+    {id:2,name:'If life were predictable it would cease to be life, and be without flavor'},
+    {id:3,name:'Life is what happens when you are busy making other plans'},
+    {id:4,name:'The way to get started is to quit talking and begin doing'}
+    ];
 
   constructor() { }
 
