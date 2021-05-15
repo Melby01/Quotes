0028a7dcaf135b5d92046332e95dadb4336cf698
Melby01 <melby.okozi@student.moringaschool.com>
2021-05-15 18:09:10 +0300

    Update the string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes = 'The Quotes';
-}
+  quotes:string[];
+
+   constructor(){
+     this.quotes = ['Your time is limited, so do not waste it living someone else life. Do not be trapped by dogma – which is living with the results of other peoples thinking', 'If life were predictable it would cease to be life, and be without flavor', 'If you look at what you have in life, you will always have more. If you look at what you do not have in life, you will never have enough', 'Life is what happens when you are busy making other plans', 'The way to get started is to quit talking and begin doing']
+   }
  
