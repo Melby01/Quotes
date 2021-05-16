a262c2c5c75b35caaa92e6a0f40bc0d26664a01d
Melby01 <melby.okozi@student.moringaschool.com>
2021-05-16 22:57:16 +0300

    Update the quote-detail.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
      this.quotes = ['Your time is limited, so do not waste it living someone else life. Do not be trapped by dogma – which is living with the results of other peoples thinking', 'If life were predictable it would cease to be life, and be without flavor', 'If you look at what you have in life, you will always have more. If you look at what you do not have in life, you will never have enough', 'Life is what happens when you are busy making other plans', 'The way to get started is to quit talking and begin doing']
    }
   }*/
-  export class AppComponent {
+  export class appComponent {
     title = 'Quotes';
     quotes = 'any quote of your choice';
   }
