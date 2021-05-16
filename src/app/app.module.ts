9edc1ee62423480a49534ade8bc026955362ae20
Melby01 <melby.okozi@student.moringaschool.com>
2021-05-16 23:40:51 +0300

    Update the app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteComponent, QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { HighestVoteDirective } from './highest-vote.directive';
@@ -13,7 +13,7 @@ import { DateCountPipe } from './date-count.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent,
+    QuoteComponent,
     QuoteDetailComponent,
     QuoteFormComponent,
     HighestVoteDirective,
