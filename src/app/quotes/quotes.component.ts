c838fc1f50b892d542c463625b47a57582b31433
Melby01 <melby.okozi@student.moringaschool.com>
2021-05-16 14:19:51 +0300

    Update the quote ts and html

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,25 +1,74 @@
 import { Component, OnInit } from '@angular/core';
-import { Quotes } from '../quotes';
-
+import { Quote } from './../quote';
 @Component({
-  selector: 'app-quotes',
-  templateUrl: './quotes.component.html',
-  styleUrls: ['./quotes.component.css']
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
 })
-export class QuotesComponent implements OnInit {
-/*
-  quotes: Quotes[] = [
-    {id:1,name:'Your time is limited, so do not waste it living someone else life. Do not be trapped by dogma – which is living with the results of other peoples thinking'},
-    {id:2,name:'If life were predictable it would cease to be life, and be without flavor'},
-    {id:3,name:'Life is what happens when you are busy making other plans'},
-    {id:4,name:'The way to get started is to quit talking and begin doing'}
-    ];
-*/
-@Input() quotes: Quotes;
+export class QuoteComponent implements OnInit {
+  quotes: Quote[] = [
+    new Quote(1, 'Your time is limited so dont waste it living someone elses life', 'by Mercy', new Date(2020, 4 , 29)),
+    new Quote(2, 'If you look at what you have in life you will always  have more', 'by Laurine', new Date(2020, 7, 21)),
+    new Quote(3, 'Life is what happens when you are busy making other plans', 'by Ludwig', new Date(2020, 7, 30)),
+    new Quote(4, 'The way to get started is to quit talking and begin doing', 'by Melby', new Date(2020, 2, 18)),
+    new Quote(5, 'If life were predictable it would cease to be life,and be without flavor', 'by Krapf', new Date(2020, 3, 19)),
+  ];
+  upVote(index) {
+    this.quotes[index].upvote++;
+  }
+  downVote(index) {
+    this.quotes[index].downvote++;
+  }
+  highestVote(){
+    let appHighestVote =  new Quote(0, '' , '', new Date());
+    for (let i = 0; i < this.quotes.length; i++) {
+      if (this.quotes[i].upvote > appHighestVote.upvote) {
+        appHighestVote = this.quotes[i];
+      }
+    }
+    if (appHighestVote.upvote > 0) {
+      return appHighestVote;
+    }else {
+      return;
+    }
+  }
+
+
+
+  // tslint:disable-next-line: typedef
+  completeQuote(isComplete, index){
+    if (isComplete) {
+      this.quotes.splice(index, 1);
+    }
+  }
+
+
+  // tslint:disable-next-line: typedef
+  deleteQuote(isComplete, index){
+    if (isComplete) {
+      // tslint:disable-next-line: prefer-const
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`);
+
+      if (toDelete){
+        this.quotes.splice(index, 1);
+      }
+    }
+  }
+  addNewQuote(quote){
+    // tslint:disable-next-line: prefer-const
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    quote.completeDate = new Date(quote.completeDate);
+    this.quotes.push(quote);
+  }
+  // tslint:disable-next-line: typedef
+  toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
-
