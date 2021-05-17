ccb91880b9b6c36b1180d0679a49640dd49c310a
Melby01 <melby.okozi@student.moringaschool.com>
2021-05-18 01:04:14 +0300

    Auto-generated commit

--- src/app/app-component.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AppComponent } from './app-component';
-
-describes('AppComponent', () => {
-  it('should create an instance', () => {
-    expect(new AppComponent()).toBeTruthy();
-  });
-});
