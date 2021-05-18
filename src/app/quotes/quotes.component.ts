e2ac42443c42b32c55f60f890ed45e61aa8d4787
Melby01 <melby.okozi@student.moringaschool.com>
2021-05-18 12:36:00 +0300

    Style the quotes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,10 +9,10 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
     new Quote (1,'Melby','The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela', new Date(2021,4,15),0,0),
-    new Quote (1,'Melby','Get busy living or get busy dying.','Chris Harvey', new Date(2021,4,28),0,0),
-    new Quote (1,'Melby','You only live once, but if you do it right, once is enough.','Labrinth James', new Date(2021,3,10),0,0),
-    new Quote (1,'Melby','The purpose of our lives is to be happy.','Marie Lorey', new Date(2019,4,23),0,0),
-    new Quote (1,'Melby','Never let the fear of striking out keep you from playing the game.','Jordan Smith', new Date(2021,4,19),0,0),
+    new Quote (2,'Cate','Get busy living or get busy dying.','Chris Harvey', new Date(2021,4,28),0,0),
+    new Quote (3,'Audrey','You only live once, but if you do it right, once is enough.','Labrinth James', new Date(2021,3,10),0,0),
+    new Quote (4,'Sabby','The purpose of our lives is to be happy.','Marie Lorey', new Date(2019,4,23),0,0),
+    new Quote (5,'Alice','Never let the fear of striking out keep you from playing the game.','Jordan Smith', new Date(2021,4,19),0,0),
     
   ];
   get sortQuotes() {
